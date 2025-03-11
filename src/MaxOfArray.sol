a30f09ca87baebc47f58dc59f444362b16f9f6b0
PablitoAmaral <pablitoamaral@gmail.com>
2025-03-11 16:11:45 +0000

    exercise 17

--- src/MaxOfArray.sol
+++ src/MaxOfArray.sol
@@ -7,6 +7,21 @@ contract MaxOfArray {
             // your code here
             // return the maximum value in the array
             // revert if array is empty
+            let x := calldataload(0x24)
+            if iszero(x){
+                revert(0x00,0x00)
+            }
+            let y := 0
+            let offset := 0x44
+            for {let z := 0} lt(z,x){z:=add(z,1)}{
+                if gt(calldataload(offset),y){
+                    y := calldataload(offset)
+                }
+                offset := add(offset,0x20)
+            }
+
+            mstore(0x00,y)
+            return(0x00,0x20)
         }
     }
 }
